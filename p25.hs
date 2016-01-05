974bade723af8391c87bf4bfaf517cf95c01df1c
patrik <patlil@kth.se>
2016-01-05 10:31:08 +0100

    fix bug

--- p25.hs
+++ p25.hs
@@ -4,10 +4,9 @@ rndPermu' :: StdGen -> [a] -> [a]
 rndPermu' _ [] = []
 rndPermu' rnd (x:xs) = let n = length xs
                            pos = head $ randomRs (0, n) rnd
-                           (hs, ts) = splitAt pos xs
-                           hsPerm = rndPermu' rnd hs
-                           tsPerm = rndPermu' rnd ts
-                       in hsPerm ++ [x] ++ tsPerm
+                           xs' = rndPermu' rnd xs
+                           (hs, ts) = splitAt pos xs'
+                       in hs ++ [x] ++ ts
 
 rndPermu xs = do g <- newStdGen
                  return (rndPermu' g xs)
